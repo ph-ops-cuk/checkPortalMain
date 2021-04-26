a139d1eaa4e710de5e56ce08116f9ea75e5ec2d3
netgod <paddy@straea.com>
2021-04-26 21:26:11 +0100

    dynamic data to dashboard from db

--- main/views.py
+++ main/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
 # Create your views here.
 from .models import *
 
+
 def home(request):
+    # full list of checks
     checklist = Check.objects.all()
-    resultlist = Result.objects.all()
     total_checks = checklist.count()
 
+    # full list of results
+    resultlist = Result.objects.all()
+    # retrieve filtered results
     totalfails = Result.objects.filter(status='Failed')
     totalfailscount = totalfails.count()
+
     totalpass = Result.objects.filter(status='Passed')
     totalpasscount = totalpass.count()
 
-    context = {'total_checks': total_checks, 'checklist': checklist, 'resultlist': resultlist, 'totalfails': totalfails, 'totalfailscount': totalfailscount, 'totalpasscount': totalpasscount}
+    test = Check.objects.filter(result__status='').count
+
+    context = {'total_checks': total_checks, 'checklist': checklist, 'resultlist': resultlist, 'totalfails': totalfails,
+               'totalfailscount': totalfailscount, 'totalpasscount': totalpasscount, 'test': test}
 
     return render(request, 'main/dashboard.html', context)
 
