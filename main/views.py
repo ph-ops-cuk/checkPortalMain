2fcdaf66d0d0e5d103326569db37dbdcf3b05a7d
netgod <paddy@straea.com>
2021-04-26 15:58:40 +0100

    start of 1st detailed templates

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'main/dashboard.html')
 
 
 def status(request):
