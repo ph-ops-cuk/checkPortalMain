bdfb1fbdbb2eb7474864aa827f336864d198810c
netgod <paddy@straea.com>
2021-04-26 13:33:34 +0100

    DB Redesign

--- main/models.py
+++ main/models.py
@@ -16,9 +16,16 @@ class Tag(models.Model):
         return self.name
 
 
+class Site(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Check(models.Model):
     name = models.CharField(max_length=200, null=True)
-    category_id = models.ForeignKey(Category, null=True, on_delete= models.SET_NULL)
+    category_id = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
     tag_id = models.ManyToManyField(Tag)
 
     def __str__(self):
@@ -30,8 +37,10 @@ class Result(models.Model):
         ('Passed', 'Passed'),
         ('Failed', 'Failed'),
     )
-    check_id = models.ForeignKey(Check, null=True, on_delete= models.SET_NULL)
+    check_id = models.ForeignKey(Check, null=True, on_delete=models.SET_NULL)
+    site_id = models.ForeignKey(Site, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=200, null=True, choices=RESULTSTATE)
+    notes = models.CharField(max_length=500, null=True)
     date_time = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
