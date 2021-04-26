89e0706332c58e7fd7eb21ad8f5cb09063325608
netgod <paddy@straea.com>
2021-04-26 17:54:30 +0100

    dynamic data to dashboard from db

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('status', views.status),
-    path('report', views.report),
+    path('', views.home, name="home"),
+    path('checks/', views.checks, name="checks"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 # Create your views here.
+from .models import *
 
 def home(request):
-    return render(request, 'main/dashboard.html')
+    checklist = Check.objects.all()
+    resultlist = Result.objects.all()
+    total_checks = checklist.count()
+
+    totalfails = Result.objects.filter(status='Failed')
+    totalfailscount = totalfails.count()
+    totalpass = Result.objects.filter(status='Passed')
+    totalpasscount = totalpass.count()
+
+    context = {'total_checks': total_checks, 'checklist': checklist, 'resultlist': resultlist, 'totalfails': totalfails, 'totalfailscount': totalfailscount, 'totalpasscount': totalpasscount}
+
+    return render(request, 'main/dashboard.html', context)
 
 
-def status(request):
-    return HttpResponse('Status')
+def checks(request):
+    return render(request, 'main/checks.html')
 
 
 def report(request):
